636aecfe37001e141dc0fae01eba64efbc541abf
Shaan H <hashmishaana@gmail.com>
2021-10-13 17:28:11 -0400

    Working Addition process

--- script.js
+++ script.js
@@ -18,12 +18,25 @@ function divi (arg1, arg2) {
 
 
 //Define object variables
-let numbers = document.querySelectorAll(".button");
+let numbers = document.querySelectorAll("#number");
 let screen = document.querySelector(".screen");
+let addButton = document.querySelector("[data-key='+']");
+let equals = document.querySelector("[data-key='=']");
+
+//debug
+let debug = document.querySelector("body");
+debug.addEventListener("click", () => {
+
+    console.log(val1, val2, func);
+
+});
+
+
 
 //Define primative variables
 let val1 = undefined;
 let val2 = undefined;
+let func = undefined;
 
 
 
@@ -31,15 +44,56 @@ let val2 = undefined;
 numbers.forEach((element) => {
 
     element.addEventListener('click', () => {
-        
 
+        
         if (screen.innerText.length <= 6) {
             screen.innerText += element.dataset.key;
+        }; 
+
+        if (val1 != undefined) {
+            val2 = screen.innerText;
         };
         
-
-
-
     });
+});
+
+//Define behaivour for add button
+addButton.addEventListener('click', () => {
+
+    if (val1 != undefined && screen.innerText != "") {
+        screen.innerText = "";
+        func = "+"
+    }; 
+    
+    if (val1 == undefined) {
+        val1 = screen.innerText;
+        screen.innerText = "";
+        func = "+"
+    };
+
+});
+
+
+
+//define behvaiour for equals button
+equals.addEventListener('click', () => {
+
+    switch(func) {
+        case "+": 
+            val2 = screen.innerText;
+            console.log(val1,val2);
+            console.log(add(val1,val2));
+            screen.innerText = add(val1,val2);
+            val1 = add(val1, val2);
+            val2 = undefined;
+            func = undefined;
+            break;  
+
+
+
+
+    }
+
+
 });
 
