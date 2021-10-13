4cfa35a0a6100ca2681ad971d62e9770ea108b7a
Shaan H <hashmishaana@gmail.com>
2021-10-13 15:11:07 -0400

    Basic Behaviour

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Define Functions
 function add (arg1, arg2) {
     return Number(arg1) + Number(arg2);
 };
@@ -13,4 +15,31 @@ function mult (arg1, arg2) {
 function divi (arg1, arg2) {
     return Number(arg1) / Number(arg2);
 };
+
+
+//Define object variables
+let numbers = document.querySelectorAll(".button");
+let screen = document.querySelector(".screen");
+
+//Define primative variables
+let val1 = undefined;
+let val2 = undefined;
+
+
+
+//Define the button event listeners
+numbers.forEach((element) => {
+
+    element.addEventListener('click', () => {
+        
+
+        if (screen.innerText.length <= 6) {
+            screen.innerText += element.dataset.key;
+        };
+        
+
+
+
+    });
+});
 
