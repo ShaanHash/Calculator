6aec4a532f4b67ef1c4a3fb270dc69dc02619220
Shaan H <hashmishaana@gmail.com>
2021-10-13 14:28:51 -0400

    basic functions added

---
+++ script.js
@@ -0,0 +1,16 @@
+function add (arg1, arg2) {
+    return Number(arg1) + Number(arg2);
+};
+
+function sub (arg1, arg2) {
+    return Number(arg1) - Number(arg2);
+};
+
+function mult (arg1, arg2) {
+    return Number(arg1) * Number(arg2);
+};
+
+function divi (arg1, arg2) {
+    return Number(arg1) / Number(arg2);
+};
+
